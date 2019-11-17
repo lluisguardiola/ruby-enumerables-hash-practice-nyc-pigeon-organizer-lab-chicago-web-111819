787e354fc7807957c623345c70fc1a26ff672fd2
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-11-17 10:21:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        new_data[name] = {} if !new_data[name]
+        new_data[name][key] = [] if !new_data[name][key]
+        new_data[name][key].push(new_value.to_s)
+        
+      end
+    end
+  end
+  
+  new_data
 end
